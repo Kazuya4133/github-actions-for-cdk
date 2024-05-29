8d4daecd0d092f178660804e76e5443410366461
Kazuya4133 <mavs4133@gmail.com>
2024-05-29 11:12:23 +0900

    YAML修正

--- infra/lib/github-actions-cdk-stack.ts
+++ infra/lib/github-actions-cdk-stack.ts
@@ -6,6 +6,11 @@ export class GithubActionsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // The code that defines your stack goes here
 
+    // example resource
+    // const queue = new sqs.Queue(this, 'GithubActionsCdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
   }
 }
