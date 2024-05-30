3573b98004b2f38652d3e68f35a96dfc3dcbe946
Kazuya4133 <mavs4133@gmail.com>
2024-05-30 11:37:37 +0900

    キャッシュのパスを変更

--- infra/lib/github-actions-cdk-stack.ts
+++ infra/lib/github-actions-cdk-stack.ts
@@ -6,6 +6,7 @@ export class GithubActionsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // The code that defines your stack goes here
 
     // example resource
     // const queue = new sqs.Queue(this, 'GithubActionsCdkQueue', {
