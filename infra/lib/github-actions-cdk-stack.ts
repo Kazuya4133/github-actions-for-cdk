28e91825f20d80ea578df4035702a7b63d156bc3
Kazuya4133 <mavs4133@gmail.com>
2024-06-05 06:04:03 +0900

    IAMロールの追加

--- infra/lib/github-actions-cdk-stack.ts
+++ infra/lib/github-actions-cdk-stack.ts
@@ -1,16 +1,29 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { CfnOIDCProvider } from 'aws-cdk-lib/aws-iam';
 
 export class GithubActionsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'GithubActionsCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const idProvider = new CfnOIDCProvider(this, 'MyOIDCProvider', {
+      url: 'https://token.actions.githubusercontent.com',
+      clientIdList: ['sts.amazonaws.com'],
+      thumbprintList: ['0123456789012345678901234567890123456789'],
+    });
+    const githubActionsRole = new iam.Role(this, 'GitHubActionsRole', {
+      roleName: `GitHubActionsRole`,
+      maxSessionDuration: cdk.Duration.hours(2),
+      assumedBy: new iam.WebIdentityPrincipal(idProvider.attrArn, {
+        StringEquals: {
+          ['token.actions.githubusercontent.com:aud']: 'sts.amazonaws.com',
+        },
+        StringLike: {
+          ['token.actions.githubusercontent.com:sub']: 'repo:Kazuya4133/github-actions-for-cdk:*',
+        },
+      }),
+    });
+    githubActionsRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'));
   }
 }
